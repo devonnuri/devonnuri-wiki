38ae9291f67dedd23a15ab1ee745904095dcb462
Huiseong Yang <me@onnuri.dev>
2024-07-10 00:50:30 +0900

    Use `outputFileTracingIncludes`

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,24 @@ import copyWebpackPlugin from 'copy-webpack-plugin';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins.push(
-        new copyWebpackPlugin({
-          patterns: [
-            {
-              from: 'mdx/',
-              to: 'mdx/',
-            },
-          ],
-        }),
-      );
-    }
+    // if (isServer) {
+    //   config.plugins.push(
+    //     new copyWebpackPlugin({
+    //       patterns: [
+    //         {
+    //           from: 'mdx/',
+    //           to: 'mdx/',
+    //         },
+    //       ],
+    //     }),
+    //   );
+    // }
     return config;
+  },
+  experimental: {
+    outputFileTracingIncludes: {
+      '/*': [`./mdx/**/*`],
+    },
   },
 };
 
