c0a3c02b76fe04f250e4b3e7ffb504f1782d46ff
Huiseong Yang <me@onnuri.dev>
2025-03-28 18:09:17 +0900

    korean as default

--- src/app/i18n/consts.ts
+++ src/app/i18n/consts.ts
@@ -4,5 +4,5 @@ export type Language = (typeof LANGUAGES)[number];
 export const checkLanguage = (lang: string): lang is Language =>
   LANGUAGES.includes(lang as Language);
 
-export const FALLBACK_LANGUAGE: Language = 'en';
+export const FALLBACK_LANGUAGE: Language = 'ko';
 export const DEFAULT_NAMESPACE = 'common';
